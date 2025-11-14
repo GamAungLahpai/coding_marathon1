4f01fbc33cebf4b5a244c581cbe1b9bcc6c86ff5
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-11-14 11:54:22 +0200

    Update inputs for email, password and nationality

--- src/components/SignupPage/SignupPage.jsx
+++ src/components/SignupPage/SignupPage.jsx
@@ -1,33 +1,63 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nationality, setNationality] = useState("");
 
-const [email, setEmail] = useState("");
+  
 
+  const handleInputChange = (event) => {
 
-const handleInputChange = (event) => {
-  setEmail(event.target.name);
-  console.log(event.target.name);
+    const value = event.target.value;
+    const name = event.target.name;
+    
+    if (name === email) {
+      setEmail(value);
+    } else if(name === password) {
+      setPassword(value);
+    } else if (name === nationality) {
+      setNationality(value);
+    }
+    console.log(value)
+  };
 
-}
+  return (
+    <section>
+      <h1>Sign Up</h1>
+      <h4>Create a free account now</h4>
+      <div>
+        <p>Email:</p>
+        <input
+          type="text"
+          name="email"
+          placeholder="john.doe@fake.com"
+          value={email}
+          onChange={handleInputChange}
+        ></input>
 
-    return(
-        <section>
-          <h1>Sign Up</h1>
-            <h4>Create a free account now</h4>
-              <div>
-                <p>Email:</p>
-                  <input
-                    type="text"
-                    name="email"
-                    placeholder="john.doe@fake.com"
-                    value={email}
-                    onChange={handleInputChange}
-                  ></input>
-              </div>
-        </section>
+        <p>Password:</p>
+        <input
+          type="password"
+          placeholder="**********"
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+        >
+        
+        </input>
 
-    );
+        <p>Nationality:</p>
+        <input
+          type="text"
+          name="nationality"
+          placeholder="Nepali"
+          value={nationality}
+          onChange={handleInputChange}
+        ></input>
+      </div>
+    </section>
+  );
 }
 
 export default SignUpPage;

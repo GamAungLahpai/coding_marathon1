587eea707f07e171fdd85f6fc42cdf6a4267f6b5
Olga <olga.chitembo@metropolia.fi>
2025-11-14 10:44:56 +0200

    Added Recipe Manager component branch

--- src/components/RecipeManager/RecipeManager.jsx
+++ src/components/RecipeManager/RecipeManager.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
 function RecipeManager() {
+  const [cuisine, setCuisine] = useState('')
+  const [difficulty, setDifficulty] = useState('')
+  const [cookTime, setCookTime] = useState('')
+  const [servings, setServings] = useState('')
+  const [allergens, setAllergens] = useState('')
+
   return (
     <div>
       RecipeManager
     </div>
+
   )
 }
  
